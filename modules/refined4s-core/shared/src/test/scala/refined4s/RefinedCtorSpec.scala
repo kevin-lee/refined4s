47b83d598d6cfa274e1dc947a001d8df611f4db1
Kevin Lee <kevin.code@kevinlee.io>
2023-12-06 11:32:53 +1100

    Close #49 - Add `RefinedCtor[T, A]` to provide a way to create a validated type `T` from an actual type `A` with validation

---
+++ modules/refined4s-core/shared/src/test/scala/refined4s/RefinedCtorSpec.scala
@@ -0,0 +1,51 @@
+package refined4s
+
+import cats.syntax.all.*
+import hedgehog.*
+import hedgehog.runner.*
+
+/** @author Kevin Lee
+  * @since 2023-12-05
+  */
+object RefinedCtorSpec extends Properties {
+  override def tests: List[Test] = List(
+    property(
+      "RefinedCtor[T].create with a valid input should return Either[String, T] = Right(T)",
+      testRefinedCtorCreate,
+    ),
+    example(
+      "RefinedCtor[T].create with an invalid input should return Either[String, T] = Left(String)",
+      testRefinedCtorCreateInvalid,
+    ),
+  )
+
+  def testRefinedCtorCreate: Property =
+    for {
+      s <- Gen.string(Gen.unicode, Range.linear(1, 10)).log("s")
+    } yield {
+      val expected = s.asRight[String]
+      val actual   = RefinedCtor[MyType, String].create(s)
+      actual ==== expected
+    }
+
+  def testRefinedCtorCreateInvalid: Result = {
+    val expected = "Invalid value: []. It has to be non-empty String but got \"\"".asLeft[MyType]
+    val actual   = RefinedCtor[MyType, String].create("")
+    actual ==== expected
+  }
+
+  type MyType = MyType.Type
+  @SuppressWarnings(Array("org.wartremover.warts.Equals"))
+  object MyType extends Refined[String] {
+
+    override inline def invalidReason(a: String): String =
+      "It has to be non-empty String but got \"" + a + "\""
+
+    override inline def predicate(a: String): Boolean = a != ""
+
+    given refinedCtor: RefinedCtor[Type, String] with {
+      override def create(a: String): Either[String, MyType] = from(a)
+    }
+  }
+
+}
