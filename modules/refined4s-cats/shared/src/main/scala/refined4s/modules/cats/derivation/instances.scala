255a65483a2ad45afca52a45613a30996a981df8
Kevin Lee <kevin.code@kevinlee.io>
2023-12-25 13:41:49 +1100

    Close #165 - Rename derived type classes - some of them have a naming conflict issue

--- modules/refined4s-cats/shared/src/main/scala/refined4s/modules/cats/derivation/instances.scala
+++ modules/refined4s-cats/shared/src/main/scala/refined4s/modules/cats/derivation/instances.scala
@@ -12,9 +12,9 @@ trait instances {
   inline def contraCoercible[F[*], A, B](fb: F[B])(using contravariant: Contravariant[F], coercible: Coercible[A, B]): F[A] =
     contravariant.contramap[B, A](fb)(coercible(_))
 
-  inline given eqDerived[A, B](using coercible: Coercible[A, B], eqB: Eq[B]): Eq[A] = contraCoercible(eqB)
+  inline given derivedEq[A, B](using coercible: Coercible[A, B], eqB: Eq[B]): Eq[A] = contraCoercible(eqB)
 
-  inline given showDerived[A, B](using coercible: Coercible[A, B], showB: Show[B]): Show[A] = contraCoercible(showB)
+  inline given derivedShow[A, B](using coercible: Coercible[A, B], showB: Show[B]): Show[A] = contraCoercible(showB)
 
 }
 object instances extends instances
