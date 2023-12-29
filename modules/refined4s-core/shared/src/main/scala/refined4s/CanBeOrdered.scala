0f815d0b2dbe06c83e6c8ed9c7c83385f5e7cd20
Kevin Lee <kevin.code@kevinlee.io>
2023-12-29 21:41:39 +1100

    Close #196 - [refined4s-core] Add CanBeOrdered for providing Ordering and Conversion[Type, Ordered[Type]]

---
+++ modules/refined4s-core/shared/src/main/scala/refined4s/CanBeOrdered.scala
@@ -0,0 +1,16 @@
+package refined4s
+
+/** @author Kevin Lee
+  * @since 2023-12-29
+  */
+trait CanBeOrdered[A: Ordering] {
+  self: NewtypeBase[A] =>
+
+  given derivedOrdering: Ordering[Type] = deriving[Ordering]
+
+  given derivedToOrdered: Conversion[Type, Ordered[Type]] with {
+    def apply(a: Type): Ordered[Type] =
+      Ordered.orderingToOrdered[Type](a)(using derivedOrdering)
+  }
+
+}
