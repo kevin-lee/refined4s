81d883cf90d26880382184768ddc9b981d88dddf
Kevin Lee <kevin.code@kevinlee.io>
2024-01-21 02:20:40 +1100

    Close #255 - [refined4s-refined-compat-scala2] Add Uri, Url and Uuid

--- modules/refined4s-refined-compat-scala2/shared/src/main/scala-2/refined4s/compat/RefinedCompatAllTypes.scala
+++ modules/refined4s-refined-compat-scala2/shared/src/main/scala-2/refined4s/compat/RefinedCompatAllTypes.scala
@@ -1,5 +1,7 @@
 package refined4s.compat
 
+import eu.timepit.refined
+import eu.timepit.refined.api.{Refined, RefinedTypeOps}
 import eu.timepit.refined.types.AllTypes
 import eu.timepit.refined.types.AllTypesBinCompat1
 import eu.timepit.refined.types.AllTypesBinCompat2
@@ -8,5 +10,29 @@ import eu.timepit.refined.types.AllTypesBinCompat3
 /** @author Kevin Lee
   * @since 2024-01-19
   */
-trait RefinedCompatAllTypes extends AllTypes with AllTypesBinCompat1 with AllTypesBinCompat2 with AllTypesBinCompat3
+trait RefinedCompatAllTypes extends AllTypes with AllTypesBinCompat1 with AllTypesBinCompat2 with AllTypesBinCompat3 {
+
+  final type Uri = allTypes.Uri
+  final val Uri = allTypes.Uri
+
+  final type Url = allTypes.Url
+  final val Url = allTypes.Url
+
+  final type Uuid = allTypes.Uuid
+  final val Uuid = allTypes.Uuid
+
+}
 object RefinedCompatAllTypes extends RefinedCompatAllTypes
+
+private[compat] object allTypes {
+
+  type Uri = String Refined refined.string.Uri
+  object Uri extends RefinedTypeOps[Uri, String]
+
+  type Url = String Refined refined.string.Url
+  object Url extends RefinedTypeOps[Url, String]
+
+  type Uuid = String Refined refined.string.Uuid
+  object Uuid extends RefinedTypeOps[Uuid, String]
+
+}
