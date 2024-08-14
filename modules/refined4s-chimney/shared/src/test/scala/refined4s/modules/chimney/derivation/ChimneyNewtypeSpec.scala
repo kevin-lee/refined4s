b5ead1535f264e2c6f3ad33b5cb958763d0395ed
Kevin Lee <kevin.code@kevinlee.io>
2024-08-15 08:03:47 +1000

    Close #340 - [`refined4s-chimney`] Add missing `Transformer` to unwrap value for `ChimneyNewtype`

--- modules/refined4s-chimney/shared/src/test/scala/refined4s/modules/chimney/derivation/ChimneyNewtypeSpec.scala
+++ modules/refined4s-chimney/shared/src/test/scala/refined4s/modules/chimney/derivation/ChimneyNewtypeSpec.scala
@@ -14,12 +14,16 @@ import refined4s.types.all.PosInt
 object ChimneyNewtypeSpec extends Properties {
 
   override def tests: List[Test] = List(
-    property("test Newtype", testNewtype),
+    property("test Newtype to Newtype with Transformer", testNewtypeToNewtype),
+    property("test Newtype to non-Newtype with Transformer", testNewtypeToNonNewtype),
     property("test Newtype[Refined]", testRefinedNewtype),
     property("test case class", testCaseClass),
+    property("test case class with Newtype to non-Newtype", testCaseClassWithNewtypeToNonNewtype),
   )
 
-  def testNewtype: Property =
+  def testNewtypeToNewtype: Property = {
+    import TestType1.*
+
     for {
       nameString <- Gen
                       .string(Gen.alpha, Range.linear(1, 10))
@@ -34,8 +38,30 @@ object ChimneyNewtypeSpec extends Properties {
       val actual = name.into[Bar.Label].transform
       expected ==== actual
     }
+  }
 
-  def testRefinedNewtype: Property =
+  def testNewtypeToNonNewtype: Property = {
+    import TestType1.*
+
+    for {
+      nameString <- Gen
+                      .string(Gen.alpha, Range.linear(1, 10))
+                      .list(Range.singleton(2))
+                      .map(_.mkString(" "))
+                      .log("nameString")
+      name = Foo.Name(nameString)
+    } yield {
+      import io.scalaland.chimney.dsl.*
+      val expected = nameString
+
+      val actual = name.into[String].transform
+      expected ==== actual
+    }
+  }
+
+  def testRefinedNewtype: Property = {
+    import TestType1.*
+
     for {
       idNum <- NumGens.genPosIntMaxTo(PosInt(Int.MaxValue)).log("idNum")
       id = Foo.Id(idNum)
@@ -46,8 +72,11 @@ object ChimneyNewtypeSpec extends Properties {
       val actual = id.into[Bar.Code].transform
       expected ==== actual
     }
+  }
 
-  def testCaseClass: Property =
+  def testCaseClass: Property = {
+    import TestType1.*
+
     for {
       idNum      <- NumGens.genPosIntMaxTo(PosInt(Int.MaxValue)).log("idNum")
       nameString <- Gen
@@ -66,28 +95,62 @@ object ChimneyNewtypeSpec extends Properties {
       val actual = Foo(id, Foo.Baz(name)).into[Bar].transform
       expected ==== actual
     }
-
-  final case class Foo(id: Foo.Id, baz: Foo.Baz)
-  object Foo {
-    type Id = Id.Type
-    object Id extends Newtype[PosInt], ChimneyNewtype[PosInt]
-
-    type Name = Name.Type
-    object Name extends Newtype[String], ChimneyNewtype[String]
-
-    final case class Baz(name: Name)
   }
 
-  final case class Bar(id: Bar.Code, baz: Bar.Baz)
+  def testCaseClassWithNewtypeToNonNewtype: Property = {
+    import TestTypes2.*
+    for {
+      nameString <- Gen
+                      .string(Gen.alpha, Range.linear(1, 10))
+                      .list(Range.singleton(2))
+                      .map(_.mkString(" "))
+                      .log("nameString")
 
-  object Bar {
-    type Code = Code.Type
-    object Code extends Newtype[PosInt], ChimneyNewtype[PosInt]
+      name = Person.Name(nameString)
+    } yield {
+      import io.scalaland.chimney.dsl.*
 
-    type Label = Label.Type
-    object Label extends Newtype[String], ChimneyNewtype[String]
+      val expected = User(nameString)
 
-    final case class Baz(name: Label)
+      val actual = Person(name).into[User].transform
+      expected ==== actual
+    }
   }
 
+  object TestType1 {
+    final case class Foo(id: Foo.Id, baz: Foo.Baz)
+
+    object Foo {
+      type Id = Id.Type
+      object Id extends Newtype[PosInt], ChimneyNewtype[PosInt]
+
+      type Name = Name.Type
+      object Name extends Newtype[String], ChimneyNewtype[String]
+
+      final case class Baz(name: Name)
+    }
+
+    final case class Bar(id: Bar.Code, baz: Bar.Baz)
+    object Bar {
+      type Code = Code.Type
+      object Code extends Newtype[PosInt], ChimneyNewtype[PosInt]
+
+      type Label = Label.Type
+      object Label extends Newtype[String], ChimneyNewtype[String]
+
+      final case class Baz(name: Label)
+    }
+  }
+
+  object TestTypes2 {
+
+    final case class Person(name: Person.Name)
+    object Person {
+      type Name = Name.Type
+
+      object Name extends Newtype[String], ChimneyNewtype[String]
+    }
+
+    final case class User(name: String)
+  }
 }
