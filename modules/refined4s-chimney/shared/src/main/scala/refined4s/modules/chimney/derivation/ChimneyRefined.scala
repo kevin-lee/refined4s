21f224e886e13f3f67d5beed5a1fc81a6fe1605d
Kevin Lee <kevin.code@kevinlee.io>
2024-08-16 22:43:29 +1000

    Close #342 - [`refined4s-chimney`] Add `PartialTransformer` to transform `Refined[A]` to `Newtype[Refined[A]]` for `ChimneyRefined`

--- modules/refined4s-chimney/shared/src/main/scala/refined4s/modules/chimney/derivation/ChimneyRefined.scala
+++ modules/refined4s-chimney/shared/src/main/scala/refined4s/modules/chimney/derivation/ChimneyRefined.scala
@@ -2,7 +2,7 @@ package refined4s.modules.chimney.derivation
 
 import io.scalaland.chimney
 import io.scalaland.chimney.{PartialTransformer, Transformer}
-import refined4s.{RefinedBase, RefinedCtor}
+import refined4s.{Coercible, RefinedBase, RefinedCtor}
 
 /** @author Kevin Lee
   * @since 2024-08-04
@@ -20,6 +20,22 @@ trait ChimneyRefined[A] {
     using refinedCtor: RefinedCtor[Type2, A]
   ): PartialTransformer[Type, Type2] = PartialTransformer[Type, Type2] { value =>
     chimney.partial.Result.fromEitherString(refinedCtor.create(value.value))
+  }
+
+  /** Refined[A] => Newtype[Refined[A]]
+    *
+    * @param anotherCoercible to create a Newtype of Newtype[Refined[A]]
+    * @param refinedCtor to create a Refined[A] of Newtype[Refined[A]]
+    * @tparam Type2 Refined of Newtype[Refined[A]]
+    * @tparam Type3 Newtype of Newtype[Refined[A]]
+    * @return
+    */
+  given wrapAToNewtypeOfRefinedForChimney[Type2, Type3](
+    using anotherCoercible: Coercible[Type2, Type3],
+    refinedCtor: RefinedCtor[Type2, A],
+  ): PartialTransformer[Type, Type3] = PartialTransformer { value =>
+    val a = value.value
+    chimney.partial.Result.fromEitherString(refinedCtor.create(a).map(anotherCoercible(_)))
   }
 
 }
