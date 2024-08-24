4d74600cf4ce6bf4c65cf42ee33f61d188b49bce
Kevin Lee <kevin.code@kevinlee.io>
2024-08-24 14:32:45 +1000

    [`refined4s-circe`] Update test structure for generic.auto

---
+++ modules/refined4s-circe/shared/src/test/scala/refined4s/modules/circe/derivation/generic/stringsSpec.scala
@@ -0,0 +1,143 @@
+package refined4s.modules.circe.derivation.generic
+
+import hedgehog.*
+import hedgehog.runner.*
+import io.circe.*
+import io.circe.parser.*
+import io.circe.syntax.*
+import refined4s.types.all.*
+
+import java.util.UUID
+
+/** @author Kevin Lee
+  * @since 2024-08-24
+  */
+object stringsSpec {
+
+  import refined4s.modules.circe.derivation.generic.auto.given
+
+  def allTests: List[Test] = List(
+    property("test Encoder[NonEmptyString]", testEncoderNonEmptyString),
+    property("test Decoder[NonEmptyString]", testDecoderNonEmptyString),
+
+    //
+    property("test Encoder[NonBlankString]", testEncoderNonBlankString),
+    property("test Decoder[NonBlankString]", testDecoderNonBlankString),
+
+    //
+    property("test Encoder[Uuid]", testEncoderUuid),
+    property("test Decoder[Uuid]", testDecoderUuid),
+  )
+
+  def testEncoderNonEmptyString: Property =
+    for {
+      s <- Gen.string(Gen.unicode, Range.linear(1, 10)).log("s")
+    } yield {
+      val input = NonEmptyString.unsafeFrom(s)
+
+      val expected = s.asJson
+      val actual   = input.asJson
+
+      Result.all(
+        List(
+          actual ==== expected,
+          actual.noSpaces ==== expected.noSpaces,
+        )
+      )
+    }
+
+  def testDecoderNonEmptyString: Property =
+    for {
+      s <- Gen.string(Gen.unicode, Range.linear(1, 10)).log("s")
+    } yield {
+      val input = s.asJson
+
+      val expected = NonEmptyString.from(s)
+      val actual   = decode[NonEmptyString](input.noSpaces)
+
+      actual ==== expected
+    }
+
+  //
+
+  def testEncoderNonBlankString: Property =
+    for {
+      nonWhitespaceString <- Gen.string(hedgehog.extra.Gens.genNonWhitespaceChar, Range.linear(1, 10)).log("nonWhitespaceString")
+      whitespaceString    <- Gen
+                               .string(
+                                 hedgehog.extra.Gens.genCharByRange(refined4s.types.strings.WhitespaceCharRange),
+                                 Range.linear(1, 10),
+                               )
+                               .log("whitespaceString")
+
+      s <- Gen.constant(scala.util.Random.shuffle((nonWhitespaceString + whitespaceString).toList).mkString).log("s")
+    } yield {
+      val input = NonBlankString.unsafeFrom(s)
+
+      val expected = s.asJson
+      val actual   = input.asJson
+
+      Result.all(
+        List(
+          actual ==== expected,
+          actual.noSpaces ==== expected.noSpaces,
+        )
+      )
+    }
+
+  def testDecoderNonBlankString: Property =
+    for {
+      nonWhitespaceString <- Gen.string(hedgehog.extra.Gens.genNonWhitespaceChar, Range.linear(1, 10)).log("nonWhitespaceString")
+      whitespaceString    <- Gen
+                               .string(
+                                 hedgehog.extra.Gens.genCharByRange(refined4s.types.strings.WhitespaceCharRange),
+                                 Range.linear(1, 10),
+                               )
+                               .log("whitespaceString")
+
+      s <- Gen.constant(scala.util.Random.shuffle((nonWhitespaceString + whitespaceString).toList).mkString).log("s")
+    } yield {
+      val input = s.asJson
+
+      val expected = NonBlankString.from(s)
+      val actual   = decode[NonBlankString](input.noSpaces)
+
+      actual ==== expected
+    }
+
+  //
+
+  @SuppressWarnings(Array("org.wartremover.warts.ToString"))
+  def testEncoderUuid: Property =
+    for {
+      uuid <- Gen.constant(UUID.randomUUID()).log("uuid")
+    } yield {
+      val s     = uuid.toString
+      val input = Uuid.unsafeFrom(s)
+
+      val expected = uuid.asJson
+      val actual   = input.asJson
+
+      Result.all(
+        List(
+          actual ==== expected,
+          actual.noSpaces ==== expected.noSpaces,
+        )
+      )
+    }
+
+  @SuppressWarnings(Array("org.wartremover.warts.ToString"))
+  def testDecoderUuid: Property =
+    for {
+      uuid <- Gen.constant(UUID.randomUUID()).log("uuid")
+    } yield {
+      val s     = uuid.toString
+      val input = s.asJson
+
+      val expected = Uuid.from(s)
+      val actual   = decode[Uuid](input.noSpaces)
+
+      actual ==== expected
+    }
+
+}
