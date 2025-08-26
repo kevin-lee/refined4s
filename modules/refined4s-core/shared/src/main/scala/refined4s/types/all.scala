d5a1c3abf4bafe216d2cef9b55f773c95ee48425
Kevin Lee <kevin.code@kevinlee.io>
2025-08-26 22:36:38 +1000

    Close #471: [`refined4s-core`] Include `refined4s.types.time` in `refined4s.types.all`

--- modules/refined4s-core/shared/src/main/scala/refined4s/types/all.scala
+++ modules/refined4s-core/shared/src/main/scala/refined4s/types/all.scala
@@ -3,5 +3,5 @@ package refined4s.types
 /** @author Kevin Lee
   * @since 2023-12-10
   */
-trait all extends numeric with strings with network
+trait all extends numeric with strings with network with time
 object all extends all
