2687c1d6f54b17db93caafc2182bc6b5a45ed1b4
Kevin Lee <kevin.code@kevinlee.io>
2025-09-03 21:39:00 +1000

    Close #504: [`refined4s-tapir`] Add `Schema` type-class instances for `refined4s.types.time` types

--- modules/refined4s-tapir/shared/src/main/scala/refined4s/modules/tapir/derivation/types/all.scala
+++ modules/refined4s-tapir/shared/src/main/scala/refined4s/modules/tapir/derivation/types/all.scala
@@ -3,5 +3,5 @@ package refined4s.modules.tapir.derivation.types
 /** @author Kevin Lee
   * @since 2024-04-03
   */
-trait all extends numeric with strings with network
+trait all extends numeric, strings, network, time
 object all extends all
