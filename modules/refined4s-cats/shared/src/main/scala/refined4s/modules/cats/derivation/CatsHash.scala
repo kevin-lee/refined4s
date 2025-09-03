6eba211359c343897b08473d885c1101f7b289ce
Kevin Lee <kevin.code@kevinlee.io>
2025-09-04 08:35:03 +1000

    Close #508: [`refined4s-cats`] Add `CatsHash` to derive `Hash` from the actual type's `Hash`

---
+++ modules/refined4s-cats/shared/src/main/scala/refined4s/modules/cats/derivation/CatsHash.scala
@@ -0,0 +1,13 @@
+package refined4s.modules.cats.derivation
+
+import cats.Hash
+import refined4s.*
+
+/** @author Kevin Lee
+  * @since 2025-09-03
+  */
+trait CatsHash[A: Hash] {
+  self: NewtypeBase[A] =>
+
+  inline given derivedHash: Hash[Type] = deriving[Hash]
+}
